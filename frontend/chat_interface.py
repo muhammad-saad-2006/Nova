1293a69b2ea86024d41af4650c0282066b7ceba7
muhammad-saad-2006 <saad2006isl@gmail.com>
2026-09-02 21:58:06 +0500

    feat: added streaming response feature

--- frontend/chat_interface.py
+++ frontend/chat_interface.py
@@ -24,12 +24,16 @@ if user_input:
         st.text(user_input)
 
     # reply from ai
-    response = chatbot.invoke({'messages': [HumanMessage(content=user_input)]}, config=CONFIG)
-
-    ai_message = response['messages'][-1].content
-
+   
+    with st.chat_message('assistant'):
+        ai_message = st.write_stream(
+            message_chunk.content for message_chunk, metadata in chatbot.stream(
+                {
+                    "messages": [HumanMessage(content=user_input)]
+                },
+                config=CONFIG,
+                stream_mode= 'messages'
+            )
+        )
 
     st.session_state['message_history'].append({'role': 'assistant', 'content': ai_message})
-
-    with st.chat_message("assistant"):
-        st.text(ai_message)
